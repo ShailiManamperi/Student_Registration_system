a000a50aacebc3fff880566f4f618907fa1acb42
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-03-16 11:36:24 +0530

    adding files to repo

---
+++ Stud_manage_system.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+from tkinter.ttk import Combobox
+import openpyxl, xlrd
+from openpyxl import Workbook
+import pathlib
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
