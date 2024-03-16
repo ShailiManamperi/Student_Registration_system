84056475653cfd1af5d37f2e66b50e7c02f6c459
ShailiManamperi <shailimanamperi2002@gmail.com>
2024-03-16 11:47:53 +0530

    making data saving filepath

--- Stud_manage_system.py
+++ Stud_manage_system.py
@@ -20,7 +20,28 @@ root.title("Student Registration System")
 root.geometry("1200x600+50+50")
 root.config(bg=background)
 
+#Creating data saving file in excel
 
+file = pathlib.Path('Student_data.xlsx')
+if file.exists():
+    pass
+else:
+    file=Workbook()
+    sheet = file.active
+    sheet['A1']="Registration No."
+    sheet['B1']="Name"
+    sheet['C1']="Class"
+    sheet['D1']="Gender"
+    sheet['E1']="DOB"
+    sheet['F1']="Date of registration"
+    sheet['G1']="Religion"
+    sheet['H1']="Skills"
+    sheet['I1']="Father's Name"
+    sheet['J1']="Mother's Name"
+    sheet['K1']="Father's Occupation"
+    sheet['L1']="Mother's Occupation"
+
+    file.save('Student_data.xlsx')
 
 
 
